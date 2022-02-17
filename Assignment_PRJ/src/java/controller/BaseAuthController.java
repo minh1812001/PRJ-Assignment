61105ab5d668462c4b432283d665dfa5b8c8dd57
minhlh1812001 <lehuuminh2001@gmail.com>
2022-02-17 23:10:51 +0700

    first commit

--- Assignment_PRJ/src/java/controller/BaseAuthController.java
+++ Assignment_PRJ/src/java/controller/BaseAuthController.java
@@ -9,6 +9,7 @@ import dalAccount.AccountDBContext;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -18,8 +19,23 @@ import model.Account;
  *
  * @author Minh-PC
  */
-public class LoginController extends HttpServlet {
 
+public abstract class BaseAuthController extends HttpServlet {
+
+    private boolean isAuthenticated(HttpServletRequest request)
+    {
+        Account account = (Account)request.getSession().getAttribute("account");
+        if(account ==null)
+            return false;
+        else
+        {
+            String url = request.getServletPath();
+            AccountDBContext db = new AccountDBContext();
+            int permission = db.getPermission(account.getUsername(), url);
+            return permission > 0;
+        }
+    }
+    
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
      * Handles the HTTP <code>GET</code> method.
@@ -32,9 +48,20 @@ public class LoginController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        request.getRequestDispatcher("login.jsp").forward(request, response);
+        if(isAuthenticated(request))
+        {
+            //business
+            processGet(request, response);
+        }
+        else
+        {
+            response.getWriter().println("access denied!");
+        }
     }
-
+    protected abstract void processGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException;
+    protected abstract void processPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException;
     /**
      * Handles the HTTP <code>POST</code> method.
      *
@@ -46,22 +73,15 @@ public class LoginController extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-           String username = request.getParameter("username");
-        String password = request.getParameter("password");
-        
-        AccountDBContext db = new AccountDBContext();
-        Account account = db.getAccount(username, password);
-        if(account == null)
+        if(isAuthenticated(request))
         {
-            request.getSession().setAttribute("account", null);
-            response.getWriter().println("login failed!");
+            //business
+            processPost(request, response);
         }
         else
         {
-            request.getSession().setAttribute("account", account);
-            response.getWriter().println("login successful! welcome " + account.getDisplayname());
+            response.getWriter().println("access denied!");
         }
-
     }
 
     /**
@@ -73,4 +93,6 @@ public class LoginController extends HttpServlet {
     public String getServletInfo() {
         return "Short description";
     }// </editor-fold>
+
+
 }
--- Assignment_PRJ/src/java/dalAccount/AccountDBContext.java
+++ Assignment_PRJ/src/java/dalAccount/AccountDBContext.java
@@ -17,18 +17,17 @@ import model.Account;
  *
  * @author Minh-PC
  */
-public class AccountDBContext extends DBContext{
-        public Account getAccount(String username, String password)
-    {
+public class AccountDBContext extends DBContext {
+
+    public Account getAccount(String username, String password) {
         try {
-            String sql = "SELECT username,password,displayname FROM Account\n" +
-                    "WHERE username = ? AND password = ?";
+            String sql = "SELECT username,password,displayname FROM Account\n"
+                    + "WHERE username = ? AND password = ?";
             PreparedStatement stm = connection.prepareStatement(sql);
             stm.setString(1, username);
             stm.setString(2, password);
             ResultSet rs = stm.executeQuery();
-            if(rs.next())
-            {
+            if (rs.next()) {
                 Account account = new Account();
                 account.setUsername(rs.getString("username"));
                 account.setPassword(rs.getString("password"));
@@ -39,5 +38,26 @@ public class AccountDBContext extends DBContext{
             Logger.getLogger(AccountDBContext.class.getName()).log(Level.SEVERE, null, ex);
         }
         return null;
+    }
+
+    public int getPermission(String username, String url) {
+        try {
+            String sql = "SELECT COUNT(*) as Total FROM\n"
+                    + "Account a INNER JOIN Account_Group ag ON a.username = ag.username\n"
+                    + "		  INNER JOIN [Group] g ON g.gid = ag.gid\n"
+                    + "		  INNER JOIN Group_Feature gf ON gf.gid = g.gid\n"
+                    + "		  INNER JOIN Feature f ON f.fid = gf.fid\n"
+                    + "WHERE a.username = ? AND f.url= ?";
+            PreparedStatement stm = connection.prepareStatement(sql);
+            stm.setString(1, username);
+            stm.setString(2, url);
+            ResultSet rs = stm.executeQuery();
+            if (rs.next()) {
+                return rs.getInt("Total");
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(AccountDBContext.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return -1;
     }
 }
